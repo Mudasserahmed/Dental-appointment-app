f8a087c8da61600e3694d14120c6c4498a3c23ab
Mudasser ahmed <mudasserahmed808@gmail.com>
2024-05-02 01:27:54 +0500

    drawer set globaly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,31 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dental Appointement App';
+  displayButton: boolean = true;
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.router.url;
+        // Check if current route is login or signup
+        if (currentRoute.includes('login') || currentRoute.includes('signUp')) {
+          this.displayButton = false;
+        } else {
+          this.displayButton = true;
+        }
+      }
+    });
+  }
+  logout(){
+    this.router.navigate(['/login'])
+    console.log("local storage cleared")
+    localStorage.clear()
+  }
+  //logo click logic
+  logoClick(){
+    console.log("image clicked")
+    this.router.navigate(["/dashboard"])
+   }
 }
