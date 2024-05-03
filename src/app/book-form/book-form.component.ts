e29268870ed9f5272aae7786a4e27a65585ebc5c
MudasserAhmed <mudasserahmed808@gmail.com>
2024-05-03 11:22:02 +0500

    calender added

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -13,12 +13,15 @@ interface  FormData   {
 })
 
 export class BookFormComponent {
+  public startDate: Date = new Date("2024-01-01");
+  public endDate: Date = new Date("2024-12-31");
+  public cssClass = "e-custom";
+
    formData:FormData = {
     fullName : "",
     phone:"",
     insurance:'',
     appDate:null
-
    }
 
    constructor(private router:Router){}
@@ -30,4 +33,10 @@ export class BookFormComponent {
     console.log("image clicked")
     this.router.navigate(["/dashboard"])
    }
+   onValueChange(args: any): void {
+    /*Displays selected date in the label*/
+    console.log(args?.value.toLocaleDateString());
+    (<HTMLInputElement>document.getElementById("selected")).textContent =
+      "Selected Value: " + args.value.toLocaleDateString();
+  }
 }
